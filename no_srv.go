438c0319f0f262fa15aa5c3f61fd87748ccfb792
AshGw <AshrefGw@proton.me>
2023-12-30 04:41:57 +0100

    hide custom `X-Powered-By` header

--- no_srv.go
+++ no_srv.go
@@ -5,6 +5,7 @@ import "net/http"
 func NoSrvMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Del("Server")
+		w.Header().Del("X-Powered-By")
 		next.ServeHTTP(w, r)
 	})
 }
